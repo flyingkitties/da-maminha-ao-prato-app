be2f0ddade83d90e9ac77f6703638b0cc802ff03
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-04-18 17:59:16 +0100

    contacts section in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,79 +8,85 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <div className="bg-secondary">
-      <div className="py-[10%]">
+    <div className=" bg-secondary">
+      <div className="grid md:grid-cols-2 py-[10%]">
         {/* Logo */}
         <div className="flexCenter">
           <Image
             alt="Logo Da Maminha ao Prato"
             src={logo}
+            className="w-[100px] h-auto"
           />
         </div>
+        {/* Info */}
         <div className="">
-          <div className="">
-            {/* Hora de atendimento */}
-            <div className="contact">
-              <div className="bannerIconDiv p-[6px] bg-secondary element">
-                <FaRegClock className="bannerIcon w-4 h-auto" />
-              </div>
-              <p>Segunda a Sexta das 09:00-17:00</p>
+          {/* Hora de atendimento */}
+          <div className="contact">
+            <div className="bannerIconDiv p-[6px] bg-secondary element">
+              <FaRegClock className="footerIcon" />
             </div>
-            {/* Telefone */}
-
-            <div className="bannerIconDiv p-[6px] element">
-              <FaPhone className="footerIcon1Small" />
-              <p>+351 935 058 203</p>
+            <p>Segunda a Sexta das 09:00-17:00</p>
+          </div>
+          {/* Telefone */}
+          <div className="contact">
+            <div className="bannerIconDiv p-[6px] bg-secondary element">
+              <FaPhone className="footerIcon" />
             </div>
-            {/* Morada */}
-            <div className="bannerIconDiv p-[6px] element">
-              <FaLocationDot className="footerIconMed" />
-              <p>Rua Blah blah blah</p>
+            <p>+351 935 058 203</p>
+          </div>
+          {/* Morada */}
+          <div className="contact">
+            <div className="bannerIconDiv p-[6px] bg-secondary element">
+              <FaLocationDot className="footerIcon" />
             </div>
-            {/* Email */}
-            <div className="bannerIconDiv p-[6px] element">
-              <FaEnvelope className="footerIconMed" />
-              <p>damaminhaaoprato@outlook.pt</p>
+            <p>Rua Blah blah blah</p>
+          </div>
+          {/* Email */}
+          <div className="contact">
+            <div className="bannerIconDiv p-[6px] bg-secondary element">
+              <FaEnvelope className="footerIcon" />
             </div>
+            <p>damaminhaaoprato@outlook.pt</p>
           </div>
         </div>
-        <div className="flex items-center justify-center py-5">
-          {/* Socials */}
-          <nav className="flex items-center justify-center space-x-5">
-            <Link
-              href="https://www.facebook.com/atelierdarelacao"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="socialsRing"
-              aria-label="o nosso facebook"
-            >
-              <FaFacebook className="footerSocials" />
-            </Link>
-            <Link
-              href="https://www.instagram.com/atelierdarelacao"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="socialsRing"
-              aria-label="o nosso instagram"
-            >
-              <PiInstagramLogoFill className="footerSocials" />
-            </Link>
-          </nav>
-        </div>
-        {/* Terms and privacy */}
-        <div className="termsAndprivacyBox">
-          <Link href="/termos-e-condicoes">
-            <p className="sm:px-5">Termos e Condições</p>
+      </div>
+
+      <div className="flex items-center justify-center py-5">
+        {/* Socials */}
+        <nav className="flex items-center justify-center space-x-5">
+          <Link
+            href="https://www.facebook.com/atelierdarelacao"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="socialsRing"
+            aria-label="o nosso facebook"
+          >
+            <FaFacebook className="footerSocials" />
           </Link>
-          <p>|</p>
-          <Link href="/politica-de-privacidade">
-            <p className="sm:px-5">Política de Privacidade</p>
+          <Link
+            href="https://www.instagram.com/atelierdarelacao"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="socialsRing"
+            aria-label="o nosso instagram"
+          >
+            <PiInstagramLogoFill className="footerSocials" />
           </Link>
-        </div>
-        {/* Copyright */}
-        <div className="copyright">
-          <p>Copyright © 2024 Atelier da Relação</p>
-        </div>
+        </nav>
+      </div>
+      {/* Terms and privacy */}
+      <div className="termsAndprivacyBox">
+        <Link href="/termos-e-condicoes">
+          <p className="sm:px-5">Termos e Condições</p>
+        </Link>
+        <p>|</p>
+        <Link href="/politica-de-privacidade">
+          <p className="sm:px-5">Política de Privacidade</p>
+        </Link>
+      </div>
+      {/* Copyright */}
+      <div className="copyright">
+        <p>Copyright © 2024 Atelier da Relação</p>
       </div>
     </div>
   );
