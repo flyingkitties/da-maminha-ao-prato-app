1f5ca010424bb68d89cabe86aa3d315df0d89025
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-04-22 18:23:11 +0100

    f/ tiles design finished

--- src/app/servicos/components/services.Tile.tsx
+++ src/app/servicos/components/services.Tile.tsx
@@ -7,7 +7,8 @@ import Link from 'next/link';
 
 function Tile({ id, image, title, priceOnline, priceHome }: Services) {
   return (
-    <div className="relative shadow-md rounded-lg min-w-72 border-2 border-secondary/30 bg-secondary/40">
+    <div className="relative shadow-md rounded-lg min-w-72 border-2 border-secondary bg-white ">
+      {/* Icon */}
       <div className="absolute -top-[40px] left-[35%] border border-secondary rounded-full p-3 bg-secondary">
         <Image
           src={image}
@@ -16,18 +17,30 @@ function Tile({ id, image, title, priceOnline, priceHome }: Services) {
           width={50}
         />
       </div>
-      <div className="py-10 px-5">
-        <div className="text-center text-lg tracking-wide text-terciary ">
+      {/* Content */}
+      <div className="pt-10 pb-5 px-5 grid grid-cols-1 space-y-5 h-full">
+        {/* Title */}
+        <div className="text-center text-lg tracking-wide text-terciary">
           <h3 className="drop-shadow-xl">{title}</h3>
         </div>
-        <FaVideo />
-        <p>{priceOnline}</p>
-        <p>{priceHome}</p>
-        <Link href={`/services/${id}`}>
-          <button className="bg-terciary/60 py-2 px-5 rounded-md text-white text-xs">
-            Mais Informações
-          </button>
-        </Link>
+        <div className="space-y-3 flex flex-col justify-end items-center">
+          {/* online */}
+          <div className="flexCenter">
+            <div className="flexCenter space-x-2 text-xs border border-terciary/60 p-1 px-3 rounded-md ">
+              <FaVideo className="text-terciary" />
+              <p>Disponível online</p>
+            </div>
+          </div>
+          {/* Button */}
+          <Link
+            className="flex items-end justify-center"
+            href={`/services/${id}`}
+          >
+            <button className="bg-terciary/60 py-2 px-5 rounded-md text-white text-xs">
+              Mais Informações
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
