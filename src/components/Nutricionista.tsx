c649a8c86c28f3732e76a2cac2d10078a5288606
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-05-08 11:23:56 +0100

    changes to nutricionista section styling

--- src/components/Nutricionista.tsx
+++ src/components/Nutricionista.tsx
@@ -5,19 +5,19 @@ import image1 from '@/../public/profile/profile-image2.png';
 function Nutricionista() {
   return (
     <div className="flexCenter sectionContainer text-darkBrown py-16">
-      <div className="grid md:grid-cols-2 sm:py-[5%] gap-5 lg:gap-10">
+      <div className="grid sm:grid-cols-2 sm:py-[5%] gap-5 lg:gap-10">
         <div className="flexCenter">
           <Image
             alt="Ana Kellen photo"
             src={image1}
-            className="w-full h-auto px-[15%] sm:px-0 lg:px-[5%]"
+            className="w-full h-auto -ml-8 px-[15%] sm:px-0 lg:px-[5%]"
           />
         </div>
         <div className="flexCenter flex-col space-y-5 lg:space-y-10">
-          <p className="font-semibold text-lg lg:text-xl xl:text-2xl text-terciary titleFont text-center md:text-left">
+          <p className="titleFont title text-center sm:text-left">
             Nutricionista materno-infantil especialista em amamentação.
           </p>
-          <p className="p-TextFormat">
+          <p className="p-TextFormat leading-relaxed sm:pr-[5%]">
             Ofereço orientação especializada em nutrição materno-infantil e
             apoio à amamentação para mães que desejam garantir o melhor começo
             de vida para o seu bebé.
